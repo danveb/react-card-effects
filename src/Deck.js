0a1f0b75ad5e912dd7e498a734e1c44fece807cd
Daniel Bae <danibae91@icloud.com>
2021-09-10 19:59:14 -0700

    implement click to keep drawing

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react' 
+import React, { useState, useEffect, useRef } from 'react' 
 import Card from './Card' 
 import axios from 'axios'
 
@@ -8,6 +8,9 @@ const Deck = () => {
     // initialize state
     const [card, setCard] = useState([])
     const [deck, setDeck] = useState(null) 
+    // state for automatic 
+    const [autoShuffle, setAutoShuffle] = useState(false) 
+    const timeId = useRef(null)
 
     useEffect(() => {
         async function getDeck() {
@@ -24,35 +27,56 @@ const Deck = () => {
         // setDeck dependency 
     }, [setDeck])
 
-    async function giveCard() {
-        // destructure deck_id from deck 
-        const { deck_id } = deck 
-        try {
-            let response = await axios.get(`${URL}/${deck_id}/draw`)
-            // console.log(response.data)
-            // console.log(response.data.cards[0])
-            let card = response.data.cards[0]
-
-            setCard(d => [
-                ...d, 
-                {
-                    id: card.code, 
-                    name: `${card.value} of ${card.suit}`,
-                    image: card.image
+    useEffect(() => {
+        async function giveCard() {
+            // destructure deck_id from deck 
+            const { deck_id } = deck 
+            try {
+                let response = await axios.get(`${URL}/${deck_id}/draw`)
+                // console.log(response.data)
+                // console.log(response.data.cards[0])
+                let card = response.data.cards[0]
+    
+                setCard(d => [
+                    ...d, 
+                    {
+                        id: card.code, 
+                        name: `${card.value} of ${card.suit}`,
+                        image: card.image
+                    }
+                ])
+    
+                // if there are no more cards left in deck
+                if(response.data.remaining === 0) {
+                    // stop drawing cards
+                    setAutoShuffle(false) 
+                    // throw error 
+                    throw new Error('no more cards left!') 
                 }
-            ])
-
-            // if there are no more cards left in deck
-            if(response.data.remaining === 0) {
-                // throw error 
-                throw new Error('no more cards left!') 
+    
+            } catch(err) {
+                alert(err)
             }
-
-        } catch(err) {
-            alert(err)
         }
-    }
 
+        if(autoShuffle && !timeId.current) {
+            timeId.current = setInterval(async () => {
+                await giveCard()
+            }, 1000)
+        }
+
+        return() => {
+            clearInterval(timeId.current) 
+            timeId.current = null 
+        }
+
+
+    }, [autoShuffle, setAutoShuffle, deck])
+
+    const toggleAutoDraw = () => {
+        setAutoShuffle(auto => !auto); 
+    }
+    
     // loop over the cards array and display each card
     const cards = card.map(c => (
         <Card key={c.id} name={c.name} image={c.image} />
@@ -60,7 +84,7 @@ const Deck = () => {
 
     return (
         <div>
-            <button onClick={(giveCard)}>Gimme a Card!</button>
+            <button onClick={(toggleAutoDraw)}>Gimme a Card!</button>
             <ul>
                 {cards}
             </ul>
